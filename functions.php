fabed7cd8672876062e493fb677592166a72a07c
Josh Pollock <jpollock412@gmail.com>
2014-05-12 23:02:20 -0400

    Add basic theme stuff from _s

--- functions.php
+++ functions.php
@@ -9,7 +9,11 @@
  * @copyright 2014 Josh Pollock
  */
 
-
+/**
+ * Switch to default theme if Pods is not active
+ *
+ * @since 0.0.1
+ */
 add_action( 'init', 'hobbes_pods_check', 1  );
 function hobbes_pods_check() {
 	if ( ! defined( 'PODS_VERSION' ) ) {
@@ -20,6 +24,11 @@ function hobbes_pods_check() {
 
 }
 
+/**
+ * Include core functions/ classes
+ *
+ * @since 0.0.1
+ */
 add_action( 'init', 'hobbes_core' );
 function hobbes_core() {
 
@@ -47,6 +56,24 @@ function hobbes_core() {
 	//Stargazer_Custom_Colors::get_instance();
 }
 
+/**
+ * Set "s_mode" to true, if not defined and include theme setup/ template tags form _s if it is true
+ *
+ * @since 0.0.1
+ */
+if ( ! defined( 'HOBBES_S_MODE' ) ) {
+	define( 'HOBBES_S_MODE', true );
+}
+
+if ( HOBBES_S_MODE ) {
+	include_once( 'lib/theme_setup/setup.php' );
+}
+
+/**
+ * Josh's debug stuff
+ *
+ * @since 0.0.1
+ */
 if ( defined( 'JP_DEBUG' ) && JP_DEBUG )  {
 	include_once( 'lib/jp_debug.php' );
 }
